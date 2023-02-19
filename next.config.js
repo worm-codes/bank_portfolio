c497c953827e0c4e7d53a4d3921599e17d8d9f6d
oguzhan.cevik <oguzhan@90pixel.com>
2023-02-19 17:08:01 +0300

    fix styled component next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+
   images: {
-    domains: ['unsplash.com', 'images.unsplash.com'],
+    domains: ['unsplash.com'],
   },
 };
 
